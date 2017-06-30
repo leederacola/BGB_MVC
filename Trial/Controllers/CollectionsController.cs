a6e180925db7b5960ec946e075d4ceac1c4173a4
leederacola <rmgforce@gmail.com>
2017-06-30 12:37:59 -0400

    Scaffold Colletion controller to begin work on adding to Library

---
+++ Trial/Controllers/CollectionsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Trial.DAL;
+using Trial.Models;
+
+namespace Trial.Controllers
+{
+    public class CollectionsController : Controller
+    {
+        private BgbContext db = new BgbContext();
+
+        // GET: Collections
+        public ActionResult Index()
+        {
+            var collections = db.Collections.Include(c => c.Game).Include(c => c.User);
+            return View(collections.ToList());
+        }
+
+        // GET: Collections/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collection collection = db.Collections.Find(id);
+            if (collection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collection);
+        }
+
+        // GET: Collections/Create
+        public ActionResult Create()
+        {
+            ViewBag.GameID = new SelectList(db.Games, "GameID", "Title");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Name");
+            return View();
+        }
+
+        // POST: Collections/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CollectionID,GameID,UserID")] Collection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Collections.Add(collection);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GameID = new SelectList(db.Games, "GameID", "Title", collection.GameID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Name", collection.UserID);
+            return View(collection);
+        }
+
+        // GET: Collections/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collection collection = db.Collections.Find(id);
+            if (collection == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GameID = new SelectList(db.Games, "GameID", "Title", collection.GameID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Name", collection.UserID);
+            return View(collection);
+        }
+
+        // POST: Collections/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CollectionID,GameID,UserID")] Collection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(collection).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GameID = new SelectList(db.Games, "GameID", "Title", collection.GameID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Name", collection.UserID);
+            return View(collection);
+        }
+
+        // GET: Collections/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collection collection = db.Collections.Find(id);
+            if (collection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collection);
+        }
+
+        // POST: Collections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Collection collection = db.Collections.Find(id);
+            db.Collections.Remove(collection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
